5107fa1c90429ead97ea6354929954be126fe674
yusriy <yusriyp@gmail.com>
2019-03-09 19:11:57 +0800

    Added two more modified and unmodified (the exploratory data analysis) courses.

---
+++ Courses/IEG301_Getting_and_Cleaning_Data/Dates_and_Times_with_lubridate/initLesson.R
@@ -0,0 +1,17 @@
+# Handles time info with ymd_hms()
+dt1 <- '2014-08-23 17:23:02'
+
+# Also handles vectors of date-times
+dt2 <- c('2014-05-14', '2014-09-22', '2014-07-11')
+
+# For last unit of the lesson
+stopwatch <- function() {
+  invisible()
+}
+
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+swirl_options(swirl_logging = TRUE)
---
+++ script/IEG301_result_analysis.R
@@ -0,0 +1,102 @@
+rm(list=ls())
+
+# Load the library
+library(swirlify)
+library(dplyr)
+
+#### Change this to go to the next name, course, lesson, etc ####
+# n_course = 1 R Programming E; n_lesson = 15
+# n_course = 2 IEG301 R Programming E; n_lesson = ???
+# n_course = 3 IEG301 Getting and Cleaning Data; n_lesson = ???
+# n_course = 4 IEG301 Exploratory Data Analysis; n_lesson = ???
+
+n_course <- 2
+n_lesson <- 14
+# Import the data
+
+#### Total scores for each lesson ####
+if (n_course == 2) {
+  total_score <- data.frame(lesson = c("basic_building_blocks", # lesson 1
+                                       "workspace_and_files", # lesson 2
+                                       "sequences_of_numbers", # lesson 3
+                                       "vectors", # lesson 4
+                                       "missing_values", # lesson 5
+                                       "subsetting_vectors", # lesson 6
+                                       "matrices_and_dataframes", # lesson 7
+                                       "logic", # lesson 8
+                                       "functions", # lesson 9
+                                       "lapply_and_sapply", # lesson 10
+                                       "vapply_and_tapply", # lesson 11
+                                       "looking_at_data", # lesson 12
+                                       "simulation", # lesson 13
+                                       "dates_and_times", # lesson 14
+                                       "base_graphics"), # lesson 15
+                            total_score = c(18,21,14,13,21,21,21,22,19,26,10,13,22,
+                                            26,19))
+}
+
+if (n_course == 3) {
+  
+}
+
+if (n_course == 4) {
+  
+}
+
+# Choose the data file to import
+df <- google_form_decode()
+
+# Convert from seconds to date
+df$datetime <- as.POSIXct(df$datetime,origin="1970-01-01")
+
+# Change some columns to factors or logicals
+df$course_name <- as.factor(df$course_name)
+df$lesson_name <- as.factor(df$lesson_name)
+df$correct <- as.logical(df$correct)
+df$skipped <- as.logical(df$skipped)
+
+# Change the lesson to tbl format
+df <- na.omit(df) # Omit all rows with NA's, usually bad data row.
+df <- tbl_df(df)
+
+# Check the list of names
+names_user <- tbl_df(unique(df$user))
+names_user <- arrange(names_user,value)
+print(names_user)
+
+# Check the list of courses
+names_courses <- tbl_df(unique(df$course_name))
+print(names_courses)
+
+# Check the list of lessons
+names_lessons <- tbl_df(unique(df$lesson_name))
+print(names_lessons)
+
+#### Score calculation ####
+# create dataframe to hold results
+df_results <- data.frame()
+df_name <- as.vector(nrow(names_user))
+df_course <- as.vector(nrow(names_user))
+df_lesson <- as.vector(nrow(names_user))
+df_score <- as.vector(nrow(names_user))
+
+for (i in 1:nrow(names_user)){
+  df_user <- df %>% filter(user == names_user$value[i]) %>%
+    filter(lesson_name == names_lessons$value[n_lesson]) %>%
+    filter(course_name == names_courses$value[n_course]) %>% 
+    filter(correct == TRUE & skipped == FALSE)
+  
+  score <- with(df_user, length(unique(question_number))) / 
+    total_score$total_score[n_lesson] * 100
+  
+  df_name[i] <- names_user$value[i]
+  df_course[i] <- as.character(names_courses$value[n_course])
+  df_lesson[i] <- as.character(names_lessons$value[n_lesson])
+  df_score[i] <- score
+  
+}
+
+df_results <- data.frame(name = df_name, course = as.character(df_course), 
+                         lesson = as.character(df_lesson), score = df_score)
+
+
