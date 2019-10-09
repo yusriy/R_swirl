27f4c3f710a721fd0d9cb7a6fd346e88808683bc
yusriy <yusriyp@gmail.com>
2019-10-10 00:17:18 +0800

    Added Intro to R course for MPOB training at Sibu.

--- Courses/Intro_R/Looking_at_Data/initLesson.R
+++ Courses/Intro_R/Looking_at_Data/initLesson.R
@@ -7,7 +7,7 @@
 
 # Path to data
 .datapath <- file.path(.get_course_path(),
-                      'R_Programming_E', 'Looking_at_Data',
+                      'Intro_R', 'Looking_at_Data',
                       'plant-data.txt')
 # Read in data
 plants <- read.csv(.datapath, strip.white=TRUE, na.strings="")
